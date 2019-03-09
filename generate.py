50e884605ad6cbe7e7946fc20048b80e0ac4b6dc
fulder <misad90@gmail.com>
2019-03-09 19:35:49 +0100

    Remove functions annotations to support older python

--- generate.py
+++ generate.py
@@ -1,4 +1,3 @@
-from __future__ import annotations
 import argparse
 import json
 import logging
@@ -10,11 +9,11 @@ logger = logging.getLogger()
 
 class PolicyGenerator:
 
-    def __init__(self, cloud_formation: dict):
+    def __init__(self, cloud_formation):
         self.cloud_formation = cloud_formation
         self.policy = {"Version": "2012-10-17", "Statements": []}
 
-    def generate(self) -> dict:
+    def generate(self):
         for resource_name in self.cloud_formation["Resources"]:
             logger.debug("Generating policy for resource: [%s]", resource_name)
 
@@ -26,7 +25,7 @@ class PolicyGenerator:
 
         return self.policy
 
-    def _s3_bucket(self, name: str, res: dict):
+    def _s3_bucket(self, name, res):
         resource = "*"
         if "BucketName" in res:
             resource = "arn:aws:s3:::{}".format(res["BucketName"])
@@ -34,7 +33,7 @@ class PolicyGenerator:
         actions = ["s3:CreateBucket", "s3:DeleteBucket"]
         self._add_policy_statement(name, res, actions, resource)
 
-    def _add_policy_statement(self, res_name: str, res: dict, actions: list, resource: str):
+    def _add_policy_statement(self, res_name, res, actions, resource):
         res_type = res["Type"].split("AWS::")[1].replace("::", "-")
         self.policy["Statements"].append({
             "Sid": "Policy-Generator-{}-{}".format(res_type, res_name),
