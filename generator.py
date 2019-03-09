37f4f6cff937d653810003d2d694046738c39e67
fulder <misad90@gmail.com>
2019-03-09 18:56:07 +0100

    Add support for AWS::S3::Bucket

--- generator.py
+++ generator.py
@@ -1,27 +1,61 @@
 import argparse
 import json
+import logging
+import time
 
 import yaml
 
+logger = logging.getLogger()
 
 class PolicyGenerator:
 
-    def __init__(self, cloudformation: dict):
-        self.cloudformation = cloudformation
+    def __init__(self, cloud_formation: dict):
+        self.cloud_formation = cloud_formation
+        self.policy: dict = {"Version": "2012-10-17", "Statements": []}
 
     def generate(self):
-        pass
+        for resource_name in self.cloud_formation["Resources"]:
+            logger.debug("Generating policy for resource: [%s]", resource_name)
+
+            res: dict = self.cloud_formation["Resources"][resource_name]
+            logger.debug("Resource type: [%s]", res["Type"])
+
+            if res["Type"] == "AWS::S3::Bucket":
+                self.s3_bucket(resource_name, res)
+
+        return self.policy
+
+    def s3_bucket(self, name, res):
+        resource = "*"
+        if "BucketName" in res:
+            resource = "arn:aws:s3:::{}".format(res["BucketName"])
+
+        actions = ["s3:CreateBucket", "s3:DeleteBucket"]
+        self._add_policy_statement(name, res, actions, resource)
+
+    def _add_policy_statement(self, res_name: str, res: dict, actions: list, resource: str):
+        res_type = res["Type"].split("AWS::")[1].replace("::", "-")
+        self.policy["Statements"].append({
+            "Sid": "Policy-Generator-{}-{}".format(res_type, res_name),
+            "Statement": {
+                "Effect": "Allow",
+                "Action": actions,
+                "Resource": resource
+            }
+        })
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Cloudformation Generator")
+    parser = argparse.ArgumentParser(description="CloudFormation Generator")
     parser.add_argument("-f", "--file", help="Path to the CloudFormation template", required=True)
     parser.add_argument("-o", "--out", help="Save output to the specified file")
     args = parser.parse_args()
 
-    cloudformation = yaml.safe_load(args.file)
-    policy = PolicyGenerator(cloudformation).generate()
+    cloud_formation_dict: dict = yaml.safe_load(open(args.file, "r"))
+    generated_policy: dict = PolicyGenerator(cloud_formation_dict).generate()
 
     if args.out is not None:
-        with open('data.txt', 'w') as outfile:
-            json.dump(policy, outfile)
+        with open(args.out, "w") as outfile:
+            json.dump(generated_policy, outfile)
+    else:
+        print(generated_policy)
--- test/test_generator.py
+++ test/test_generator.py
@@ -1,7 +1,52 @@
 import unittest
 
+from generator import PolicyGenerator
+
 
 class TestGenerator(unittest.TestCase):
 
     def setUp(self):
-        PolicyGenerator()
+        self.cloudformation: dict = {
+            "AWSTemplateFormatVersion": "2010-09-09"
+        }
+
+    def test_s3_bucket(self):
+        self.cloudformation["Resources"] = {
+            "TestBucket": {
+                "Type": "AWS::S3::Bucket"
+            }
+        }
+        policy = PolicyGenerator(self.cloudformation).generate()
+        expected_policy = {
+            "Version": "2012-10-17",
+            "Statements": [{
+                "Sid": "Policy-Generator-S3-Bucket-TestBucket",
+                "Statement": {
+                    "Effect": "Allow",
+                    "Action": ["s3:CreateBucket", "s3:DeleteBucket"],
+                    "Resource": "*"
+                }
+            }]
+        }
+        self.assertEqual(policy, expected_policy)
+
+    def test_s3_bucket_with_name(self):
+        self.cloudformation["Resources"] = {
+            "TestBucket": {
+                "Type": "AWS::S3::Bucket",
+                "BucketName": "my-test-bucket"
+            }
+        }
+        policy = PolicyGenerator(self.cloudformation).generate()
+        expected_policy = {
+            "Version": "2012-10-17",
+            "Statements": [{
+                "Sid": "Policy-Generator-S3-Bucket-TestBucket",
+                "Statement": {
+                    "Effect": "Allow",
+                    "Action": ["s3:CreateBucket", "s3:DeleteBucket"],
+                    "Resource": "arn:aws:s3:::my-test-bucket"
+                }
+            }]
+        }
+        self.assertEqual(policy, expected_policy)
