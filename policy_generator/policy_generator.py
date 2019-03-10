e325c7d76243ae1aa061c3a9586608eb55b73a28
fulder <misad90@gmail.com>
2019-03-10 01:52:26 +0100

    Add support for AWS::Lambda::Function

--- policy_generator/policy_generator.py
+++ policy_generator/policy_generator.py
@@ -5,8 +5,10 @@ logger = logging.getLogger()
 
 class PolicyGenerator:
 
-    def __init__(self, cloud_formation):
+    def __init__(self, cloud_formation, region, account):
         self.cloud_formation = cloud_formation
+        self.region = region
+        self.account = account
         self.policy = {"Version": "2012-10-17", "Statements": []}
 
     def generate(self):
@@ -18,6 +20,8 @@ class PolicyGenerator:
 
             if res["Type"] == "AWS::S3::Bucket":
                 self._s3_bucket(resource_name, res)
+            if res["Type"] == "AWS::Lambda::Function":
+                self._lambda_function(resource_name, res)
 
         return self.policy
 
@@ -29,10 +33,26 @@ class PolicyGenerator:
         actions = ["s3:CreateBucket", "s3:DeleteBucket"]
         self._add_policy_statement(name, res, actions, resource)
 
-    def _add_policy_statement(self, res_name, res, actions, resource):
+    def _lambda_function(self, name, res):
+        resource = "*"
+        actions = ["lambda:CreateFunction"]
+        if "VpcConfig" in res:
+            actions += ["ec2:AllocateAddress", "ec2:AssociateAddress", "ec2:DisassociateAddress"]
+        self._add_policy_statement(name, res, actions, resource, name_suffix="Global")
+
+        resource = "*"
+        if "FunctionName" in res:
+            resource = "arn:aws:lambda:{0}:{1}:function:{2}".format(self.region, self.account, res["FunctionName"])
+        actions = ["lambda:DeleteFunction"]
+        self._add_policy_statement(name, res, actions, resource)
+
+    def _add_policy_statement(self, res_name, res, actions, resource, name_suffix=None):
         res_type = res["Type"].split("AWS::")[1].replace("::", "-")
+        sid = "Policy-Generator-{0}-{1}".format(res_type, res_name)
+        if name_suffix is not None:
+            sid += "-" + name_suffix
         self.policy["Statements"].append({
-            "Sid": "Policy-Generator-{0}-{1}".format(res_type, res_name),
+            "Sid": sid,
             "Statement": {
                 "Effect": "Allow",
                 "Action": actions,
--- test/test_generator.py
+++ test/test_generator.py
@@ -7,46 +7,141 @@ class TestGenerator(unittest.TestCase):
 
     def setUp(self):
         self.cloudformation = {
-            "AWSTemplateFormatVersion": "2010-09-09"
+            "AWSTemplateFormatVersion": "2010-09-09",
+            "Resources": {}
         }
 
     def test_s3_bucket(self):
         self.cloudformation["Resources"] = {
-            "TestBucket": {
+            "TestResource": {
                 "Type": "AWS::S3::Bucket"
             }
         }
-        policy = PolicyGenerator(self.cloudformation).generate()
+        policy = PolicyGenerator(self.cloudformation, "us-east-1", "ABC123123").generate()
         expected_policy = {
             "Version": "2012-10-17",
-            "Statements": [{
-                "Sid": "Policy-Generator-S3-Bucket-TestBucket",
-                "Statement": {
-                    "Effect": "Allow",
-                    "Action": ["s3:CreateBucket", "s3:DeleteBucket"],
-                    "Resource": "*"
-                }
-            }]
+            "Statements": [
+                {
+                    "Sid": "Policy-Generator-S3-Bucket-TestResource",
+                    "Statement": {
+                        "Effect": "Allow",
+                        "Action": ["s3:CreateBucket", "s3:DeleteBucket"],
+                        "Resource": "*"
+                    }
+                }]
         }
         self.assertEqual(policy, expected_policy)
 
     def test_s3_bucket_with_name(self):
         self.cloudformation["Resources"] = {
-            "TestBucket": {
+            "TestResource": {
                 "Type": "AWS::S3::Bucket",
                 "BucketName": "my-test-bucket"
             }
         }
-        policy = PolicyGenerator(self.cloudformation).generate()
+        policy = PolicyGenerator(self.cloudformation, "us-east-1", "ABC123123").generate()
         expected_policy = {
             "Version": "2012-10-17",
-            "Statements": [{
-                "Sid": "Policy-Generator-S3-Bucket-TestBucket",
-                "Statement": {
-                    "Effect": "Allow",
-                    "Action": ["s3:CreateBucket", "s3:DeleteBucket"],
-                    "Resource": "arn:aws:s3:::my-test-bucket"
+            "Statements": [
+                {
+                    "Sid": "Policy-Generator-S3-Bucket-TestResource",
+                    "Statement": {
+                        "Effect": "Allow",
+                        "Action": ["s3:CreateBucket", "s3:DeleteBucket"],
+                        "Resource": "arn:aws:s3:::my-test-bucket"
+                    }
+                }]
+        }
+        self.assertEqual(policy, expected_policy)
+
+    def test_lambda(self):
+        self.cloudformation["Resources"] = {
+            "TestResource": {
+                "Type": "AWS::Lambda::Function"
+            }
+        }
+        policy = PolicyGenerator(self.cloudformation, "us-east-1", "ABC123123").generate()
+        expected_policy = {
+            "Version": "2012-10-17",
+            "Statements": [
+                {
+                    "Sid": "Policy-Generator-Lambda-Function-TestResource-Global",
+                    "Statement": {
+                        "Effect": "Allow",
+                        "Action": ["lambda:CreateFunction"],
+                        "Resource": "*"
+                    }
+                },
+                {
+                    "Sid": "Policy-Generator-Lambda-Function-TestResource",
+                    "Statement": {
+                        "Effect": "Allow",
+                        "Action": ["lambda:DeleteFunction"],
+                        "Resource": "*"
+                    }
                 }
-            }]
+            ]
+        }
+        self.assertEqual(policy, expected_policy)
+
+    def test_lambda_with_name(self):
+        self.cloudformation["Resources"] = {
+            "TestResource": {
+                "Type": "AWS::Lambda::Function",
+                "FunctionName": "LambdaName"
+            }
+        }
+        policy = PolicyGenerator(self.cloudformation, "us-east-1", "ABC123123").generate()
+        expected_policy = {
+            "Version": "2012-10-17",
+            "Statements": [
+                {
+                    "Sid": "Policy-Generator-Lambda-Function-TestResource-Global",
+                    "Statement": {
+                        "Effect": "Allow",
+                        "Action": ["lambda:CreateFunction"],
+                        "Resource": "*"
+                    }
+                },
+                {
+                    "Sid": "Policy-Generator-Lambda-Function-TestResource",
+                    "Statement": {
+                        "Effect": "Allow",
+                        "Action": ["lambda:DeleteFunction"],
+                        "Resource": "arn:aws:lambda:us-east-1:ABC123123:function:LambdaName"
+                    }
+                }
+            ]
+        }
+        self.assertEqual(policy, expected_policy)
+
+    def test_vpc_lambda(self):
+        self.cloudformation["Resources"] = {
+            "TestResource": {
+                "Type": "AWS::Lambda::Function",
+                "VpcConfig": "TEMP"
+            }
+        }
+        policy = PolicyGenerator(self.cloudformation, "us-east-1", "ABC123123").generate()
+        expected_policy = {
+            "Version": "2012-10-17",
+            "Statements": [
+                {
+                    "Sid": "Policy-Generator-Lambda-Function-TestResource-Global",
+                    "Statement": {
+                        "Effect": "Allow",
+                        "Action": ["lambda:CreateFunction", "ec2:AllocateAddress", "ec2:AssociateAddress", "ec2:DisassociateAddress"],
+                        "Resource": "*"
+                    }
+                },
+                {
+                    "Sid": "Policy-Generator-Lambda-Function-TestResource",
+                    "Statement": {
+                        "Effect": "Allow",
+                        "Action": ["lambda:DeleteFunction"],
+                        "Resource": "*"
+                    }
+                }
+            ]
         }
         self.assertEqual(policy, expected_policy)
